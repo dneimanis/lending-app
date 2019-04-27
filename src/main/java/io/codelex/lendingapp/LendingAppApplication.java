dcd8623894d5002db53d0607b793a3fae7a5f730
dneimanis <davis.neimanis@gmail.com>
2019-04-27 14:13:18 +0300

    initial commit

---
+++ src/main/java/io/codelex/lendingapp/LendingAppApplication.java
@@ -0,0 +1,13 @@
+package io.codelex.lendingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingAppApplication.class, args);
+	}
+
+}
